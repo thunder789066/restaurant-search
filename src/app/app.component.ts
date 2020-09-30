cc28a4284b75688f29c5ee557ec5dd000a4d7370
Christina Gerstner <christinalgerstner@gmail.com>
2020-09-30 00:04:21 -0500

    updated to working state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+// import {pipeline} from 'stream';
+// import {mainDiagnosticsForTest} from '@angular/compiler-cli/src/main';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'restaurant-search';
+  title = 'Restaurant Search';
+  city = '';
+  food = '';
+  venues = [];
+
+  constructor(private _http: HttpClient) {}
+
+  submit() {
+    let url: string;
+    url = "https://api.foursquare.com/v2/venues/search";
+    url += "?near=" + this.city;
+    url += "&radius=8047";
+    url += "&limit=5";
+    url += "&query=" + this.food;
+    url += "&client_id=IZCDV1MCZBCVIHTZDS1ZABOYL1L4QSLQYYJJUN0VTAJJF2NM";
+    url += "&client_secret=WEATVV2IY2YT00DS04CS41F52RWVVMHYTKLIVOY0EVH5QISE";
+    url += "&v=20200929";
+
+    this._http.get(url)
+       .subscribe(data => {
+         this.venues = data.response.venues;
+         console.log(this.venues);
+       });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientJsonpModule, HttpClientModule } from "@angular/common/http";
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    HttpClientJsonpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
